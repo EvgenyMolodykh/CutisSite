aaf668299d02b3b35eb43a682a36608f3fb250ff
Evgeny Molodykh <ololo128666@gmail.com>
2024-06-20 23:42:14 +0500

    Начало разработки

---
+++ script/scrtipt.js
@@ -0,0 +1,5 @@
+const img = document.getElementById('animated-img');
+
+window.onload = function() {
+  img.style.transform = 'translateY(0%)'; // Выдвигаем изображение обратно вверх при загрузке страницы
+};
